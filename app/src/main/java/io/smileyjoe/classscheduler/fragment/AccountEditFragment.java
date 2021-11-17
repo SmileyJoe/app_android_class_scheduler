b717a9fb049ef060cdf47f38391bad3760dba91e
Cody Strugnell <cody.strugnell@gmail.com>
2021-11-17 18:33:23 +0200

    Adding profile image picker

--- app/src/main/java/io/smileyjoe/classscheduler/fragment/AccountEditFragment.java
+++ app/src/main/java/io/smileyjoe/classscheduler/fragment/AccountEditFragment.java
@@ -1,20 +1,39 @@
 package io.smileyjoe.classscheduler.fragment;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.navigation.Navigation;
 
+import com.canhub.cropper.CropImageContract;
+import com.canhub.cropper.CropImageContractOptions;
+import com.canhub.cropper.CropImageOptions;
+import com.canhub.cropper.CropImageView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.File;
+import java.io.IOException;
 
 import io.smileyjoe.classscheduler.R;
 import io.smileyjoe.classscheduler.database.DbUser;
@@ -25,6 +44,7 @@ import io.smileyjoe.classscheduler.utils.Communication;
 public class AccountEditFragment extends BaseFirebaseFragment<FragmentAccountEditBinding> implements DatabaseReference.CompletionListener {
 
     private User mUser;
+    private ActivityResultLauncher<CropImageContractOptions> mCropLauncher;
 
     @Override
     protected FragmentAccountEditBinding inflate(LayoutInflater inflater, ViewGroup container, boolean savedInstanceState) {
@@ -35,7 +55,41 @@ public class AccountEditFragment extends BaseFirebaseFragment<FragmentAccountEdi
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        setupActivityResults();
         getRoot().buttonSave.setOnClickListener(v -> save());
+        getRoot().imageProfile.setOnClickListener(v -> {
+            CropImageContractOptions options = new CropImageContractOptions(null, new CropImageOptions())
+                    .setScaleType(CropImageView.ScaleType.CENTER)
+                    .setCropShape(CropImageView.CropShape.OVAL)
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1,1)
+                    .setMaxZoom(8)
+                    .setAutoZoomEnabled(true)
+                    .setAllowFlipping(false)
+                    .setActivityTitle(getString(R.string.menu_profile_image))
+                    .setActivityMenuIconColor(ContextCompat.getColor(getContext(), R.color.colorPrimary))
+                    .setCropMenuCropButtonIcon(R.drawable.ic_check)
+                    .setAllowRotation(false)
+                    .setFixAspectRatio(true);
+
+            mCropLauncher.launch(options);
+
+        });
+    }
+
+    private void setupActivityResults(){
+        mCropLauncher = registerForActivityResult(
+                new CropImageContract(),
+                result -> {
+                    if (result.isSuccessful()) {
+                        getRoot().imageProfile.setScaleType(ImageView.ScaleType.CENTER);
+                        getRoot().imageProfile.setImageTintList(null);
+                        getRoot().imageProfile.setImageURI(result.getUriContent());
+                    } else {
+                        error(R.string.error_image_select);
+                    }
+                }
+        );
     }
 
     @Override
